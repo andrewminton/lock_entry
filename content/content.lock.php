e84871e249209c65be296dcd22259529bd5ee3a8
Andrew Minton <me@andrewminton.co.uk>
2012-06-10 15:56:14 +0100

    Initial code

---
+++ content/content.lock.php
@@ -0,0 +1,64 @@
+<?php
+
+	require_once(TOOLKIT . '/class.administrationpage.php');
+	require_once(TOOLKIT . '/class.extensionmanager.php');
+	require_once(TOOLKIT . '/class.sectionmanager.php');
+	
+	class ContentExtensionLock_EntryLock extends AdministrationPage {
+		
+		protected $_driver = null;
+		
+		public function __construct(){
+			parent::__construct();
+			$this->_uri = SYMPHONY_URL . '/extension/lock_entry';
+			$this->_driver = Symphony::ExtensionManager()->create('lock_entry');
+		}
+		
+		public function __viewIndex() {
+			
+			$timeout = 30;
+			$entry_id = $_GET['entry_id'];
+			$getuser = Administration::instance()->Author;
+			$user_id = '1';
+		    //var_dump($getuser,$user_id,$entry_id);
+			
+			
+			switch($_GET['lock']) {
+				
+				case 'checkLocked':
+				
+					$lock = Symphony::Database()->fetchRow(0, "SELECT * FROM tbl_entry_lock WHERE entry_id=$entry_id");
+					$locked = false;
+					
+					// page has been locked by someone else
+					if ($lock && $lock['user_id'] = $user_id) {
+						
+						// lock has expired, remove it for housekeeping
+						if ((time() - strtotime($lock['timestamp'])) >= $timeout) {
+							Symphony::Database()->query("DELETE FROM tbl_entry_lock WHERE entry_id=$entry_id");
+						} else {
+							$locked = true;
+						}
+                         
+					}
+					
+					header('content-type: text/javascript');
+					echo 'EntryLock.is_locked = ' . (($locked) ? 'true' : 'false') . ';';
+					
+				break;
+				
+				case 'lockEntry':
+					$lock = Symphony::Database()->fetchRow(0, "SELECT * FROM tbl_entry_lock WHERE entry_id=$entry_id");
+					if ($lock) Symphony::Database()->query("DELETE FROM tbl_entry_lock WHERE entry_id=$entry_id");
+					Symphony::Database()->query("INSERT INTO tbl_entry_lock (entry_id, user_id) VALUES ($entry_id, $user_id)");			
+					
+					header('content-type: text/javascript');
+					echo 'Entry Has been Locked';
+					
+				break;
+			}
+			
+			exit;
+			
+		}
+	}
---
+++ extension.driver.php
@@ -0,0 +1,43 @@
+<?php
+	
+	require_once(TOOLKIT . '/class.sectionmanager.php');
+	
+	class Extension_Lock_Entry extends Extension {
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'initaliseAdminPageHead'
+				)
+			);
+		}
+		
+		public function initaliseAdminPageHead($context) {
+	    
+			require_once(TOOLKIT . '/class.sectionmanager.php');
+		
+			$page = Administration::instance()->Page;
+			$pageContext = $page->getContext();
+			$entryID = $pageContext['entry_id'];
+			
+			if ($page instanceof ContentPublish and ($pageContext['page'] == 'new' || $pageContext['page'] == 'edit')) {
+				$page->addScriptToHead(URL . '/extensions/lock_entry/assets/lock_entry.js', 300000);
+			    $page->addScriptToHead(SYMPHONY_URL . '/extension/lock_entry/lock/?lock=checkLocked&entry_id='.$entryID, 300001);
+			}
+		}
+		
+		public function uninstall(){
+			return Symphony::Database()->query("DROP TABLE `tbl_entry_lock`");
+		}
+
+
+		public function install(){
+			return Symphony::Database()->query("CREATE TABLE `tbl_entry_lock` (
+			  `entry_id` int(11) NOT NULL,
+			  `user_id` int(11) default NULL,
+			  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+		}
+}
